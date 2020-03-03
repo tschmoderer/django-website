02189d7e9ae6045f1bee1d05736cb2e7d6da588a
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-03 13:47:59 +0100

    before sass test

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import datetime
+
+def home(request):
+	return render(request, 'blog/tpl.html', {'current_date':datetime.now()})
