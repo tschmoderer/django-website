09747e5f8fbc8a01d2a7fda63d945eb0b440ae11
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-04 16:16:50 +0100

    update home page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path(r'', views.home),
+	path(r'about', views.home), 
+	path(r'about.html', views.home),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from datetime import datetime
 
 def home(request):
-	return render(request, 'blog/tpl.html', {'current_date':datetime.now()})
+	return render(request, 'blog/about.html')
