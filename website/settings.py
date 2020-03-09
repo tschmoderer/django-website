b4a6866d40c9aef590a6b4dc14248579fe70395e
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-09 14:26:00 +0100

    update static directories

--- website/settings.py
+++ website/settings.py
@@ -112,21 +112,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+USE_I18N  = True
+USE_L10N  = True
+USE_TZ    = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL  = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL  = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets/'),
 ]
