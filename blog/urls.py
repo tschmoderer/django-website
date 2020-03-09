4314a4d2f5ffe1cfb1349a197d42204c03e6d869
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-09 08:52:09 +0100

    update blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-	path(r'', views.home),
-	path(r'about', views.home, name="about"), 
-	path(r'about.html', views.home),
+	path(r'', views.home, name='blog'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from datetime import datetime
-
 def home(request):
 	return render(request, 'blog/about.html')
