29a203633ee814e5bdca8132be325bec6441aeba
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-12 09:40:18 +0100

    create a model for homepage

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,11 @@
+#-*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Homepage(models.Model): 
+    title   = models.CharField(max_length=100)
+    content = models.TextField(null=True)
+    date_modif = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name="Modification date")
+
+    def __unicode__(self):
+        return u"%s" % self.title 
