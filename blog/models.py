afecaedc22fad8a3e1967eb90e59ce098d7a1853
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-12 09:40:08 +0100

    add description field to model

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,9 @@ class Article(models.Model):
     title   = models.CharField(max_length=100)
     author  = models.CharField(max_length=42)
     content = models.TextField(null=True)
-    date_publi = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Publication date")
-    date_modif = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name="Modification date")
+    description = models.TextField(null=True)
+    date_publi  = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Publication date")
+    date_modif  = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name="Modification date")
 
     def __unicode__(self):
         return u"%s" % self.title 
