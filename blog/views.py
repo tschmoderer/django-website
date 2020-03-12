ff8c69b798b00fdf6733347e86450ca323338d0d
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-12 18:11:50 +0100

    clean views

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from blog.models import Article
 
 def home(request):
-	articles = Article.objects.all()
+	articles = [] # Article.objects.all()
 	return render(request, 'blog/home.html', {'last_articles':articles})
 
 def read(request, id):
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,29 @@
 #-*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .models import Homepage
 
 def home(request):
 	return render(request, 'homepage/home.html')
+
+def edit(request):
+	query = Homepage.objects.all()
+	context = {
+		"homepage": query,
+	}
+	return render(request, 'homepage/edit.html', context)
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'signup.html', {'form': form})
