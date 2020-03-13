47da80feabcad355e30e9e314f4b6d39f7dacbfa
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-13 08:58:56 +0100

    support for login/logout

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	# re_path(r'edit/', views.edit, name='edit'),
 	# re_path(r'signup/', views.signup, name='signup'),
 	re_path(r'login/', auth_views.LoginView.as_view(template_name='homepage/login.html'), name='login'),
-	re_path(r'logout/', auth_views.LogoutView.as_view(template_name='homepage/logout.html'), name='logout'),
+	re_path(r'logout/', auth_views.LogoutView.as_view(template_name='homepage/home.html'), name='logout'),
 ]
