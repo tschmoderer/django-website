2d92f5f952bb020b05a22a72cbc65b3d055fee75
Timothée Schmoderer <timothee.schmoderer@netcourrier.com>
2020-03-18 10:06:28 +0100

    update user form

--- homepage/forms.py
+++ homepage/forms.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
-        model   = User
-        exclude = ('username', 'password', 'date_joined')
+        model  = User
+        fields = ('first_name', 'last_name', 'email', )
 
 class HomepageForm(forms.ModelForm):
     class Meta:
